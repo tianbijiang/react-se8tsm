ade2bd5dd29cd27ee16ec1a5daa1f908259f2f1d
tianbijiang <tianbi.jiang@gmail.com>
2019-11-14 18:11:05 -0600

    1 file modified

--- reducerCreator.js
+++ reducerCreator.js
@@ -5,7 +5,7 @@ export default function (reducerFunction, secReducer, reducerName) {
         if (state === undefined) {
           return secReducer ? "default2" : "default1";
         }
-        if (action.payload && action.payload.secReducer !== secReducer && state!==undefined) {
+        if (action.payload && action.payload.secReducer !== secReducer) {
           return state;
         }
         return reducerFunction(state, action);
