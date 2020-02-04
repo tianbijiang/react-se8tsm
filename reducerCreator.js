eba8746f296384afb441acaa3f7ed173f5d93f8d
tianbijiang <tianbi.jiang@gmail.com>
2020-02-04 11:30:36 -0600

    1 file modified

--- reducerCreator.js
+++ reducerCreator.js
@@ -1,13 +1,28 @@
 // purpose: to reuse value1Reducer.js
-export default function (reducerFunction, secReducer, reducerName) {
-    return (state, action) => {
-        reducerFunction.reducerName = reducerName;
-        if (state === undefined) {
-          return secReducer ? "default2" : "default1";
-        }
-        if (action.payload && action.payload.secReducer !== secReducer) {
-          return state;
-        }
-        return reducerFunction(state, action);
-    };
-};
+export default function(reducerFunction, secReducer, reducerName) {
+  return (state, action) => {
+    reducerFunction.reducerName = reducerName;
+    if (state === undefined) {
+      return secReducer ? "default2" : "default1";
+    }
+
+    const source =
+      action.secReducer ||
+      (action.payload && action.payload.secReducer) ||
+      false;
+    if (secReducer && source) {
+      const inputState = state;
+      const newState = reducerFunction(inputState, action);
+      if (inputState !== newState) {
+        return newState;
+      }
+      return state;
+    }
+
+    if (!source && secReducer === false) {
+      return reducerFunction(state, action);
+    }
+
+    return state;
+  };
+}
